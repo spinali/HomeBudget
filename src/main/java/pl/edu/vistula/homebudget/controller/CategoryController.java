9316fe02a08d6461ad09c28cd15e1704a0e18be3
spinali <rikperche@hotmail.com>
2025-01-15 21:42:47 +0100

    create, find, findAll, delete

--- src/main/java/pl/edu/vistula/homebudget/controller/CategoryController.java
+++ src/main/java/pl/edu/vistula/homebudget/controller/CategoryController.java
@@ -5,12 +5,12 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.vistula.homebudget.api.request.CategoryRequest;
 import pl.edu.vistula.homebudget.api.response.CategoryResponse;
 import pl.edu.vistula.homebudget.service.CategoryService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/categories")
@@ -24,5 +24,23 @@ public class CategoryController {
     public ResponseEntity<CategoryResponse> create(@RequestBody CategoryRequest categoryRequest) {
         CategoryResponse categoryResponse = categoryService.create(categoryRequest);
         return new ResponseEntity<>(categoryResponse, HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}")
+    @Operation(summary = "find category by id")
+    public ResponseEntity<CategoryResponse> findById(@PathVariable Long id) {
+        CategoryResponse categoryResponse = categoryService.find(id);
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
+    }
+    @GetMapping
+    @Operation(summary = "find all categories")
+    public ResponseEntity<List<CategoryResponse>> findAll() {
+        List<CategoryResponse> categoryResponses = categoryService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(categoryResponses);
+    }
+    @DeleteMapping("/{id}")
+    @Operation(summary = "delete category by id")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        categoryService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
